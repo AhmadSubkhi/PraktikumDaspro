d3c21ff2d9f46f8b43bf7813f3805b90be6e2318
AhmadSubkhi <254107020155@student.polinema.ac.id>
2025-10-01 13:43:08 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS03.java
+++ jobsheet5/ifCetakKRS03.java
@@ -8,12 +8,16 @@ public class ifCetakKRS03 {
         System.out.print("Apakah UKT sudah lunas? (true/false)");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+        // if (uktLunas) {
+        //     System.out.println("Pembayaran UKT terverifikasi");
+        //     System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        // } else {
+        //     System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        // }
+
+        System.out.print(uktLunas 
+        ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" 
+        : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
 
         sc.close();
 
